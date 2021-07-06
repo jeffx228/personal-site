fdb91759e9bb01850f41dbac37439346f1dad041
Jeffrey Xu <jeffrey@xuyaowu.com>
2021-07-06 09:42:08 -0700

    stat changes

--- src/data/stats/site.js
+++ src/data/stats/site.js
@@ -16,30 +16,6 @@ const data = [
     link: 'https://github.com/mldangelo/personal-site/stargazers',
   },
   {
-    label: 'Number of forks',
-    key: 'forks',
-    link: 'https://github.com/mldangelo/personal-site/network',
-  },
-  {
-    label: 'Number of spoons',
-    value: '0',
-  },
-  {
-    label: 'Number of linter warnings',
-    value: '0', // enforced via github workflow
-  },
-  {
-    label: 'Open github issues',
-    key: 'open_issues_count',
-    link: 'https://github.com/mldangelo/personal-site/issues',
-  },
-  {
-    label: 'Last updated at',
-    key: 'pushed_at',
-    link: 'https://github.com/mldangelo/personal-site/commits',
-    format: (x) => dayjs(x).format('MMMM DD, YYYY'),
-  },
-  {
     // TODO update this with a pre-commit hook
     /* find . | grep ".js" | grep -vE ".min.js|node_modules|.git|.json" |
     xargs -I file cat file | wc -l */
